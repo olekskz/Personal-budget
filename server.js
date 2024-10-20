ee79f57853c894ca7e4758ba3e73e07076ca7271
olekskz <134650124+olekskz@users.noreply.github.com>
2024-10-20 20:34:32 +0200

    server added body div made

--- server.js
+++ server.js
@@ -1,14 +1,12 @@
 const express = require('express')
 const app = express()
-
 const PORT = 3001;
 
 
-app.get('/', (req, res) => {
-    res.send('Hello world')
-})
+app.use(express.static('public'))
 
 
 app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`)
 })
+
