b494cf7ecd1b96fa8bbc10be0a1f936ad0cbcbe5
olekskz <134650124+olekskz@users.noreply.github.com>
2024-10-20 13:02:09 +0200

    header added

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ const app = express()
 
 const PORT = 3001;
 
+
+app.get('/', (req, res) => {
+    res.send('Hello world')
+})
+
+
 app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`)
 })
